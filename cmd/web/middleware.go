9ea8e232dd2039f7c9d891685d4a5719d3418d77
0xshiku <olachico@icloud.com>
2024-12-03 20:43:12 +0000

    feat: introduce panic recovery

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -29,5 +32,25 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event of a panic as Go unwinds the stack)
+		defer func() {
+			// Use the builtin recover function to check if there has been a panic or not. If there has..
+			if err := recover(); err != nil {
+				// Set a "Connection: close" header on the response.
+				// This acts as a trigger to make Go's HTTP server automatically close the current connection after a response has been sent.
+				// It also informs the user that the connection will be closed.
+				// Note: If the protocol being used is HTTP/2, Go will automatically strip the connection: close header from the response
+				// So it is not malformed, and send a GOAWAY frame.
+				w.Header().Set("Connection", "close")
+				// Call the app.serverError helper method to return a 500
+				// Internal server response
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
 	})
 }
