0301c83fa4364ac3829fe527fcbfd0d73a3c06e9
0xshiku <olachico@icloud.com>
2024-12-13 23:21:03 +0000

    feat: redirect user after login

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -64,6 +64,7 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// If the user is not authenticated, redirect them to the login page and return from the middleware chain so that no subsequent handlers in the chain are executed.
 		if !app.isAuthenticated(r) {
+			app.sessionManager.Put(r.Context(), "redirectPathAfterLogin", r.URL.Path)
 			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 			return
 		}
